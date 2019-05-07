86d0acba0919f753d28e8941fe00527e6376656b
unknown <tarek.ahmed2009@yahoo.com>
2019-05-07 05:49:54 +0200

    expression validation and evaluation not completed yet

---
+++ include/ExpressionEvaluator.h
@@ -0,0 +1,21 @@
+#ifndef EXPRESSIONEVALUATOR_H
+#define EXPRESSIONEVALUATOR_H
+#include <instructions.h>
+#include <singleton.h>
+
+class ExpressionEvaluator
+{
+    public:
+        ExpressionEvaluator();
+        pair<bool,string> checkOperand(Instructions& i);
+    protected:
+    private:
+        Singleton* tables;
+        bool checkExist(string operand);
+        bool isOperations(string operation);
+        int get_priority(string operation);
+        pair<bool,pair<int,string>>validateOperandsTypeEvaluation(pair<int,string>operandOne,
+                                                pair<int,string>operandTwo,string operation);
+};
+
+#endif // EXPRESSIONEVALUATOR_H
